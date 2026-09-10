0917c1c5581d35e6b769c8eb50663ee8fab2dec9
Dell Office Rudra <rudrapatel2542005@gmail.com>
2026-09-10 17:12:45 +0530

    test: provide ThemeCubit and AppModuleCubit for NbCrmApp widget test

--- nb_crm_flutter/test/widget_test.dart
+++ nb_crm_flutter/test/widget_test.dart
@@ -1,12 +1,28 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:go_router/go_router.dart';
+import 'package:nb_crm_flutter/core/bloc/app_module_cubit.dart';
+import 'package:nb_crm_flutter/core/theme/theme_cubit.dart';
 import 'package:nb_crm_flutter/main.dart';
 
 void main() {
   testWidgets('App boots under ProviderScope', (tester) async {
     final router = GoRouter(routes: []);
-    await tester.pumpWidget(ProviderScope(child: NbCrmApp(router: router)));
+    final themeCubit = ThemeCubit();
+    final appModuleCubit = AppModuleCubit();
+
+    await tester.pumpWidget(
+      MultiBlocProvider(
+        providers: [
+          BlocProvider<ThemeCubit>.value(value: themeCubit),
+          BlocProvider<AppModuleCubit>.value(value: appModuleCubit),
+        ],
+        child: ProviderScope(
+          child: NbCrmApp(router: router),
+        ),
+      ),
+    );
     await tester.pump();
     expect(find.byType(NbCrmApp), findsOneWidget);
   });
